979bc10490a17aa2f9af8e1111823633aff97b5c
alumno <jj>
2023-02-15 20:01:52 +0100

    "Añadidos botones de volver"

--- app/src/main/java/com/example/pmdm_practica3/Estudiante.java
+++ app/src/main/java/com/example/pmdm_practica3/Estudiante.java
@@ -2,13 +2,27 @@ package com.example.pmdm_practica3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Estudiante extends AppCompatActivity {
 
+    Button main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estudiante);
+
+        main = (Button) findViewById(R.id.atrasBtn);
+        main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Estudiante.this, MainActivity.class);
+                startActivity(intent);
+                onStop();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/pmdm_practica3/Intereses.java
+++ app/src/main/java/com/example/pmdm_practica3/Intereses.java
@@ -2,13 +2,27 @@ package com.example.pmdm_practica3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Intereses extends AppCompatActivity {
 
+    Button main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intereses);
+
+        main = (Button) findViewById(R.id.atrasBtn4);
+        main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intereses.this, MainActivity.class);
+                startActivity(intent);
+                onStop();
+            }
+        });
     }
 }
